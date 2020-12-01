5137f5990dab79bc17274810a59f050928e5e6be
Jillian S <jilliansn@gmail.com>
2020-12-01 12:24:45 -0700

    moved banner to render props for flexibility on a page-by-page basis.

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,20 +1,29 @@
 import React, { FC, ReactNode } from 'react'
 
 
-const Banner: FC<ReactNode> = ({}) => {
+export type BannerProps = {
+  title?: ReactNode,
+  subTitle?: ReactNode,
+  [other:string]: unknown
+}
+
+const Banner: FC<BannerProps> = ({
+  title,
+  subTitle
+}) => {
   return (
     <React.Fragment>
       <div className="bg-indigo-400 bg-opacity-30 w-full">
-        <div className="flex flex-col justify-center items-center max-w-screen-lg m-auto h-80 pb-20">
-          <h1 className="text-gray-50 text-4xl font-light">
+        <div className="flex flex-col justify-center items-center max-w-screen-lg m-auto h-80 pb-20 px-6">
+          <h1 className="text-gray-50 text-4xl font-light text-center">
             {/* <span
               className="bg-gradient-to-r text-transparent bg-clip-text from-rose-300 via-lightBlue-300 to-teal-300"
               style={{color: 'transparent'}}
             > */}
-              <i className="fal fa-narwhal text-rose-300"></i>&nbsp;Bit-Casting Technomancer&nbsp;<i className="fal fa-alien-monster text-teal-300"></i>
+            {title}
             {/* </span> */}
           </h1>
-          <p className="text-gray-50 text-2xl font-light mt-3">Designer, Full Stack Developer, &amp; Tech Lead</p>
+          <p className="text-gray-50 text-2xl font-light mt-3 tracking-wide text-center">{subTitle}</p>
         </div>
       </div>
     </React.Fragment>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,47 +8,52 @@ import Avatar from '../components/avatar'
 const Home: FC<ReactNode> = () => {
   return (
     <Layout>
-      <Banner />
+      <Banner
+        title={
+          <><i className="fal fa-narwhal text-rose-300"></i>&nbsp;Bit-Casting Technomancer&nbsp;<i className="fal fa-alien-monster text-teal-300"></i></>
+        }
+        subTitle={<>Designer, Full Stack Developer, &amp; Tech Lead</>}
+      />
       <main className="min-h-screen max-w-screen-lg m-auto">
         <section className="bg-gray-100 rounded-xl p-8 w-2/3 mx-auto -mt-12 mb-10 shadow-xl relative">
           <Avatar />
-          <h2 className="text-gray-800 text-2xl mt-6">About Me</h2>
-          <p className="text-gray-600 tracking-wider mt-4">
+          <h2 className="text-gray-800 text-2xl mt-6 tracking-wide">About Me</h2>
+          <p className="text-gray-600 tracking-wider mt-3">
             Greetings! My name is Jillian (she/her). I'm a software developer based in Colorado, USA.
           </p>
           <p className="text-gray-600 tracking-wider mt-4">
             My interests in software development lie at the intersection of improving developer experience, designer experience, <em>and</em> <a href="https://medium.com/s/user-friendly/why-im-done-saying-user-user-experience-and-ux-in-2019-4fdfc6b7de23" target="_blank" rel="noopener noreferrer">audience&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> experience. I work on innovative approaches to software and design such as <i className="fas fa-heart" style={{ color: 'pink' }}></i> <a href="https://www.invisionapp.com/inside-design/guide-to-design-systems/" target="_blank" rel="noopener noreferrer">design dystems&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>, <a href="https://medium.com/omarelgabrys-blog/component-based-architecture-3c3c23c7e348" target="_blank" rel="noopener noreferrer">component based&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>, <a href="https://martinfowler.com/articles/serverless.html" target="_blank" rel="noopener noreferrer">serverless&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>, <a href="https://martinfowler.com/articles/microservices.html" target="_blank" rel="noopener noreferrer"> and microservice&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> architectures, and the beloved <i className="fas fa-heart" style={{ color: 'pink' }}></i> <a href="https://jamstack.org/" target="_blank" rel="noopener noreferrer">Jamstack&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>.
           </p>
 
-          <h3 className="text-gray-700 text-xl mt-10">Current Work</h3>
-          <p className="text-gray-600 tracking-wider mt-4">
+          <h3 className="text-gray-700 text-xl mt-10 tracking-wide">Current Work</h3>
+          <p className="text-gray-600 tracking-wider mt-3">
             I work for a nonprofit organization, <a href="https://bscs.org/" target="_blank" rel="noopener noreferrer">BSCS Science Learning&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>, whose mission is to transform science teaching and learning through research-driven innovation.
           </p>
           <p className="text-gray-600 tracking-wider mt-4">
             At BSCS Science Learning, we operate on federal and private grants. The nature of this work requires our small team to build many websites and web apps every year. Implementing our infrastructure to be agile enough to meet the needs of a diverse range of projects while staying within budget, meeting strict deadlines, and reducing the cost of ongoing maintenance (technical debt) is paramount.
           </p>
 
-          <h3 className="text-gray-700 text-xl mt-10">Previous Work</h3>
-          <p className="text-gray-600 tracking-wider mt-4">
+          <h3 className="text-gray-700 text-xl mt-10 tracking-wide">Previous Work</h3>
+          <p className="text-gray-600 tracking-wider mt-3">
             Prior to my career as a software developer, I was an astronomy and physics instructor. My courses introduced students to the skills required to think critically about the physical world and offered students experience in solving real-world problems.
           </p>
           <p className="text-gray-600 tracking-wider mt-4">
             In graduate school, I studied the space environment of planets and comets. Colloquially known as “space weather,” my studies investigated how the solar wind affects the magnetospheres, ionospheres, and upper atmospheres of these objects.
           </p>
 
-          <h3 className="text-gray-700 text-xl mt-10">Education</h3>
-          <p className="text-gray-600 tracking-wider mt-4">
+          <h3 className="text-gray-700 text-xl mt-10 tracking-wide">Education</h3>
+          <p className="text-gray-600 tracking-wider mt-3">
             I hold an <a href="https://en.wikipedia.org/wiki/Master_of_Science#United_States" target="_blank" rel="noopener noreferrer">M.S.&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> in <a href="https://clasp.engin.umich.edu/" target="_blank" rel="noopener noreferrer">Atmospheric and Space Science&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> and a <a href="https://en.wikipedia.org/wiki/Graduate_certificate#United_States" target="_blank" rel="noopener noreferrer">GradCert&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> in <a href="https://micde.umich.edu/" target="_blank" rel="noopener noreferrer">Computer Science and Engineering&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> from <a href="https://umich.edu/" target="_blank" rel="noopener noreferrer">The University of Michigan, Ann Arbor&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>. I did my undergraduate studies at <a href="https://www.colorado.edu/" target="_blank" rel="noopener noreferrer">The University of Colorado, Boulder&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> where I earned a <a href="https://en.wikipedia.org/wiki/Bachelor%27s_degree" target="_blank" rel="noopener noreferrer">B.A.&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> in <a href="https://www.colorado.edu/aps/" target="_blank" rel="noopener noreferrer">Astrophysical, Planetary, and Atmospheric Science&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> and a <a href="https://en.wikipedia.org/wiki/Bachelor%27s_degree" target="_blank" rel="noopener noreferrer">B.A.&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a> in <a href="https://www.colorado.edu/physics/" target="_blank" rel="noopener noreferrer">Physics&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>.
           </p>
 
-          <h3 className="text-gray-700 text-xl mt-10">Extracurricular Interests</h3>
-          <p className="text-gray-600 tracking-wider mt-4">
+          <h3 className="text-gray-700 text-xl mt-10 tracking-wide">Extracurricular Interests</h3>
+          <p className="text-gray-600 tracking-wider mt-3">
             In my free time I enjoy adventuring in my van (<a href="https://www.instagram.com/explore/tags/vanlife/?hl=en" target="_blank" rel="noopener noreferrer">#vanlife&nbsp;<sup><i className="fas fa-external-link-alt external-link-icon" /></sup></a>), spending time with my dog, practicing yoga, mountain biking, and reading.
           </p>
         </section>
 
         <section className="bg-gray-100 rounded-xl p-8 pb-10 w-2/3 mx-auto mb-20 shadow-xl z-10 relative">
-          <h2 className="text-gray-800 text-2xl">Say Hello</h2>
+          <h2 className="text-gray-800 text-2xl tracking-wide">Say Hello</h2>
           <div className="grid grid-cols-1 gap-6 mt-8">
             <label className="block">
               <span className="text-gray-600 tracking-wider">Name</span>
