fa786aabe2a0137033e9ed79661d77696398e923
Jillian S <jilliansn@gmail.com>
2020-11-30 23:35:14 -0700

    Fized z-indexing nightmare

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,11 +5,11 @@ const Header: FC<ReactNode> = ({}) => {
   return (
     <React.Fragment>
       <header className="bg-indigo-400 bg-opacity-30 w-full py-6 px-8">
-        <div className="flex justify-center items-center justify-between max-w-screen-lg m-auto">
+        <div className="flex items-center justify-between max-w-screen-lg m-auto">
           <div className="logo z-5">
             <p className="js text-4xl text-gray-50 font-semibold">JS</p>
           </div>
-          <nav className="nav flex justify-center items-center justify-between text-gray-50">
+          <nav className="nav flex items-center justify-between text-gray-50">
             <p className="text-base tracking-wider">Writing</p>
             <p className="ml-6 text-base tracking-wider">Speaking</p>
             <p className="ml-6 text-base tracking-wider">Projects</p>
