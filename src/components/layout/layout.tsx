dc3a507c390b98042d70eb1543b7f10e420ba7f5
Jillian <jillianserenity@gmail.com>
2020-12-18 01:18:21 -0700

    Fully finished inished refactoring memphis.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -10,11 +10,11 @@ import {
 } from '../../utils/utils'
 
 import {
-  // createSquaresSolid,
-  createSquaresOutline,
-  createTriangles,
-  createBolts,
-  createWave,
+  squareSolid,
+  squareOutline,
+  triangles,
+  bolts,
+  waves,
   createShapeElement
 } from '../../utils/memphis'
 
@@ -33,107 +33,26 @@ const Layout: FC<LayoutProps> = ({
   const [height, setHeight] = useState(getDocumentHeight)
   const [width, setWidth] = useState(getWindowInnerWidth)
 
-  const squareSolid = {
-    bordered: false,
-    colors: [
-      '#FB7185', // rose 400
-      '#F472B6', // pink 400
-      '#38BDF8' // lightBlue 400
-    ],
-    degrees: 90,
-    icon: false,
-    innerClassArray: ['inner-square-solid'],
-    innerElementType: 'div',
-    max: 220,
-    min: 60,
-    outerClassArray: ['outer-square-solid'],
-    outerElementType: 'div'
-  }
-
-  const squareOutline = {
-    bordered: true,
-    colors: [
-      '#5EEAD4', // teal 300
-      '#FAFAF9' // warm gray 50
-    ],
-    icon: false,
-    innerClassArray: ['inner-square-outline'],
-    innerElementType: 'div',
-    max: 120,
-    min: 20,
-    outerClassArray: ['outer-square-outline'],
-    outerElementType: 'div'
-  }
-
-  const triangles = {
-    bordered: true,
-    colors: [
-      '#A78BFA', // violet 400
-      '#38BDF8', // lightBlue 400
-    ],
-    icon: true,
-    innerClassArray: ['inner-triangle', `fal`, `fa-triangle`],
-    innerElementType: 'i',
-    max: 100,
-    min: 40,
-    outerClassArray: ['outer-triangle'],
-    outerElementType: 'div'
-  }
-
-  const bolts = {
-    bordered: true,
-    colors: [
-      '#FEF08A', // yellow 200
-      '#67E8F9', // cyan 300
-    ],
-    icon: true,
-    innerClassArray: ['inner-bolt', `fal`, `fa-bolt`],
-    innerElementType: 'i',
-    max: 80,
-    min: 40,
-    outerClassArray: ['outer-bolt'],
-    outerElementType: 'div'
-  }
-
-  const waves = {
-    bordered: true,
-    colors: [
-      '#1C1917' // warm gray 900
-    ],
-    icon: true,
-    innerClassArray: ['inner-wave', `fal`, `fa-water`],
-    innerElementType: 'i',
-    max: 60,
-    min: 20,
-    outerClassArray: ['outer-wave'],
-    outerElementType: 'div'
-  }
-
   useEffect(() => {
     setTimeout(() => {
       // Initialize the solid squares
       for (let i = 0; i < 10; i++) {
-        // createSquaresSolid()
         createShapeElement(squareSolid)
       }
       // Initialize the outline squares
       for (let i = 0; i < 7; i++) {
-        // createSquaresOutline()
         createShapeElement(squareOutline)
       }
       // Initialize the triangles
       for (let i = 0; i < 7; i++) {
-        // createTriangles()
         createShapeElement(triangles)
       }
       // Initialize the bolts
       for (let i = 0; i < 5; i++) {
-        // createBolts()
         createShapeElement(bolts)
       }
       // Initialize the waves
       for (let i = 0; i < 20; i++) {
-        // createWave()
         createShapeElement(waves)
       }
     }, 350)
@@ -168,27 +87,22 @@ const Layout: FC<LayoutProps> = ({
     const populateElements = () => {
       // Re-draw the solid squares
       for (let i = 0; i < 10; i++) {
-        // createSquaresSolid()
         createShapeElement(squareSolid)
       }
       // Re-draw the outline squares
       for (let i = 0; i < 7; i++) {
-        // createSquaresOutline()
         createShapeElement(squareOutline)
       }
       // Re-draw the triangles
       for (let i = 0; i < 7; i++) {
-        // createTriangles()
         createShapeElement(triangles)
       }
       // Re-draw the bolts
       for (let i = 0; i < 5; i++) {
-        // createBolts()
         createShapeElement(bolts)
       }
       // Re-draw the waves
       for (let i = 0; i < 20; i++) {
-        // createWave()
         createShapeElement(waves)
       }
     }
--- src/utils/memphis.ts
+++ src/utils/memphis.ts
@@ -3,164 +3,81 @@ import {
   getDocumentHeight
 } from './utils'
 
-/*
-  Create the set of solid squares
-*/
-// export const createSquaresSolid = () => {
-//   let height: number = getDocumentHeight()
+export const squareSolid = {
+  bordered: false,
+  colors: [
+    '#FB7185', // rose 400
+    '#F472B6', // pink 400
+    '#38BDF8' // lightBlue 400
+  ],
+  degrees: 90,
+  icon: false,
+  innerClassArray: ['inner-square-solid'],
+  innerElementType: 'div',
+  max: 220,
+  min: 60,
+  outerClassArray: ['outer-square-solid'],
+  outerElementType: 'div'
+}
 
-//   const colors: string[] = [
-//     '#FB7185', // rose 400
-//     '#F472B6', // pink 400
-//     '#38BDF8' // lightBlue 400
-//   ]
-//   const canvas: HTMLElement | null = document.getElementById('canvas')
+export const squareOutline = {
+  bordered: true,
+  colors: [
+    '#5EEAD4', // teal 300
+    '#FAFAF9' // warm gray 50
+  ],
+  icon: false,
+  innerClassArray: ['inner-square-outline'],
+  innerElementType: 'div',
+  max: 120,
+  min: 20,
+  outerClassArray: ['outer-square-outline'],
+  outerElementType: 'div'
+}
 
-//   const size: number = randomIntFromInterval(60, 220)
-//   const bg: string = colors[Math.floor(Math.random() * colors.length)]
+export const triangles = {
+  bordered: true,
+  colors: [
+    '#A78BFA', // violet 400
+    '#38BDF8', // lightBlue 400
+  ],
+  icon: true,
+  innerClassArray: ['inner-triangle', `fal`, `fa-triangle`],
+  innerElementType: 'i',
+  max: 100,
+  min: 40,
+  outerClassArray: ['outer-triangle'],
+  outerElementType: 'div'
+}
 
-//   const outerSquareSolid: HTMLElement = document.createElement('div')
-//   outerSquareSolid.classList.add('outer-square-solid')
-//   outerSquareSolid.style.width = size + 'px'
-//   outerSquareSolid.style.height = size + 'px'
+export const bolts = {
+  bordered: true,
+  colors: [
+    '#FEF08A', // yellow 200
+    '#67E8F9', // cyan 300
+  ],
+  icon: true,
+  innerClassArray: ['inner-bolt', `fal`, `fa-bolt`],
+  innerElementType: 'i',
+  max: 80,
+  min: 40,
+  outerClassArray: ['outer-bolt'],
+  outerElementType: 'div'
+}
 
-//   const innerSquareSolid: HTMLElement = document.createElement('div')
-//   innerSquareSolid.classList.add('inner-square-solid')
-//   innerSquareSolid.style.width = size + 'px'
-//   innerSquareSolid.style.height = size + 'px'
-//   innerSquareSolid.style.top = randomIntFromInterval(0, height).toString() + 'px'
-//   innerSquareSolid.style.left = randomIntFromInterval(0, innerWidth).toString() + 'px'
-//   innerSquareSolid.style.transform = `rotate(${Math.floor(Math.random() * 90)}deg)`
-//   innerSquareSolid.style.background = bg
-
-//   canvas.appendChild(outerSquareSolid)
-//   outerSquareSolid.appendChild(innerSquareSolid)
-// }
-/*
-  Create the set of outline squares
-*/
-// export const createSquaresOutline = () => {
-//   let height: number = getDocumentHeight()
-
-//   const colors: string[] = [
-//     '#5EEAD4', // teal 300
-//     '#FAFAF9' // warm gray 50
-//   ]
-//   const canvas: HTMLElement | null = document.getElementById('canvas')
-
-//   const size: number = randomIntFromInterval(20, 120)
-//   const bg: string = colors[Math.floor(Math.random() * colors.length)]
-
-//   const outerSquareOutline: HTMLElement = document.createElement('div')
-//   outerSquareOutline.classList.add('outer-square-outline')
-//   outerSquareOutline.style.width = size + 'px'
-//   outerSquareOutline.style.height = size + 'px'
-
-//   const innerSquareOutline: HTMLElement = document.createElement('div')
-//   innerSquareOutline.classList.add('inner-square-outline')
-//   innerSquareOutline.style.width = size + 'px'
-//   innerSquareOutline.style.height = size + 'px'
-//   innerSquareOutline.style.top = randomIntFromInterval(0, height).toString() + 'px'
-//   innerSquareOutline.style.left = randomIntFromInterval(0, innerWidth).toString() + 'px'
-//   innerSquareOutline.style.borderColor = bg
-
-//   canvas.appendChild(outerSquareOutline)
-//   outerSquareOutline.appendChild(innerSquareOutline)
-// }
-/*
-  Create the set of triangles
-*/
-// export const createTriangles = () => {
-//   let height: number = getDocumentHeight()
-
-//   const colors: string[] = [
-//     '#A78BFA', // violet 400
-//     '#38BDF8', // lightBlue 400
-//   ]
-//   const canvas: HTMLElement | null = document.getElementById('canvas')
-
-//   const size: number = randomIntFromInterval(40, 100)
-//   const bg: string = colors[Math.floor(Math.random() * colors.length)]
-
-//   const outerTriangle: HTMLElement = document.createElement('div')
-//   outerTriangle.classList.add('outer-triangle')
-//   outerTriangle.style.fontSize = size + 'px'
-
-//   const innerTriangle: HTMLElement = document.createElement('i')
-//   innerTriangle.classList.add(`inner-triangle`, `fal`, `fa-triangle`)
-//   innerTriangle.style.fontSize = size + 'px'
-//   innerTriangle.style.top = randomIntFromInterval(0, height).toString() + 'px'
-//   innerTriangle.style.left = randomIntFromInterval(0, innerWidth).toString() + 'px'
-//   innerTriangle.style.color = bg
-
-//   canvas.appendChild(outerTriangle)
-//   outerTriangle.appendChild(innerTriangle)
-// }
-/*
-  Create the set of bolts
-*/
-// export const createBolts = () => {
-//   let height: number = getDocumentHeight()
-
-//   const colors: string[] = [
-//     '#FEF08A', // yellow 200
-//     '#67E8F9', // cyan 300
-//   ]
-//   const canvas: HTMLElement | null = document.getElementById('canvas')
-
-//   const size: number = randomIntFromInterval(40, 80)
-//   const bg: string = colors[Math.floor(Math.random() * colors.length)]
-
-//   const outerBolt: HTMLElement = document.createElement('div')
-//   outerBolt.classList.add('outer-bolt')
-//   outerBolt.style.fontSize = size + 'px'
-
-//   const innerBolt: HTMLElement = document.createElement('i')
-//   innerBolt.classList.add(`inner-bolt`, `fal`, `fa-bolt`)
-//   innerBolt.style.fontSize = size + 'px'
-//   innerBolt.style.top = randomIntFromInterval(0, height).toString() + 'px'
-//   innerBolt.style.left = randomIntFromInterval(0, innerWidth).toString() + 'px'
-//   innerBolt.style.color = bg
-
-//   canvas.appendChild(outerBolt)
-//   outerBolt.appendChild(innerBolt)
-// }
-/*
-  Create the set of wave triangle
-*/
-// export const createWave = () => {
-//   let height: number = getDocumentHeight()
-
-//   const colors: string[] = [
-//     '#1C1917' // warm gray 900
-//   ]
-//   const canvas: HTMLElement | null = document.getElementById('canvas')
-
-//   const size: number = randomIntFromInterval(20, 60)
-//   const bg: string = colors[Math.floor(Math.random() * colors.length)]
-
-//   const outerWave: HTMLElement = document.createElement('div')
-//   outerWave.classList.add('outer-wave')
-//   outerWave.style.fontSize = size + 'px'
-
-//   const innerWave: HTMLElement = document.createElement('i')
-//   innerWave.classList.add(`inner-wave`, `fal`, `fa-water`)
-//   innerWave.style.fontSize = size + 'px'
-//   innerWave.style.top = randomIntFromInterval(0, height).toString() + 'px'
-//   innerWave.style.left = randomIntFromInterval(0, innerWidth).toString() + 'px'
-//   innerWave.style.color = bg
-
-//   canvas.appendChild(outerWave)
-//   outerWave.appendChild(innerWave)
-// }
-
-
-
-
-
-
-
-
+export const waves = {
+  bordered: true,
+  colors: [
+    '#1C1917' // warm gray 900
+  ],
+  icon: true,
+  innerClassArray: ['inner-wave', `fal`, `fa-water`],
+  innerElementType: 'i',
+  max: 60,
+  min: 20,
+  outerClassArray: ['outer-wave'],
+  outerElementType: 'div'
+}
 
 export interface ShapeElementParameters {
   bordered: boolean,
