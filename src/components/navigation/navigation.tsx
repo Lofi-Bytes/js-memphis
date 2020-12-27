498b493de4ed7f10058e2c6da9f7c330140baaa9
Jillian <jillianserenity@gmail.com>
2020-12-27 01:30:21 -0700

    Refined navigation menus.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -35,32 +35,32 @@ const Navigation: FC<NavigationProps> = ({
             return(
               <React.Fragment key={`item-${index}`}>
                 <div className="ml-6">
-                  {/* <Link
-                    to={path}
-                    className="p-1 text-base tracking-wider text-teal-100 duration-200 rounded hover:text-pink-200 focus:text-pink-200 active:text-pink-300 hover:cursor-pointer focus:outline-none focus:ring focus:ring-teal-200"
-                  >
-                    {title}
-                  </Link> */}
+                  {
+                    location.pathname.match(regex)
+                    ? (
+                      <>
+                        <Link
+                          to={path}
+                          className="p-1 text-base tracking-wider text-teal-100 duration-200 rounded hover:text-pink-200 focus:text-pink-200 active:text-pink-300 hover:cursor-pointer focus:outline-none focus:ring focus:ring-teal-200"
+                        >
+                          {title}
+                        </Link>
+                        <div className="border-b-2 border-pink-200 rounded" />
+                      </>
+                    )
+                    : (
+                      <>
+                        <Link
+                          to={path}
+                          className="p-1 text-base tracking-wider text-teal-100 duration-200 rounded hover:text-pink-200 focus:text-pink-200 active:text-pink-300 hover:cursor-pointer focus:outline-none focus:ring focus:ring-teal-200"
+                        >
+                          {title}
+                        </Link>
+                        <div className="border-b-2 rounded" style={{ borderColor: 'transparent' }} />
+                      </>
+                    )
+                  }
                 </div>
-                {
-                  location.pathname.match(regex)
-                  ? (
-                    <Link
-                      to={path}
-                      className="p-1 text-base tracking-wider text-pink-200 duration-200 rounded hover:text-pink-200 focus:text-pink-200 active:text-pink-300 hover:cursor-pointer focus:outline-none focus:ring focus:ring-teal-200"
-                    >
-                      {title}
-                    </Link>
-                  )
-                  : (
-                    <Link
-                      to={path}
-                      className="p-1 text-base tracking-wider text-teal-100 duration-200 rounded hover:text-pink-200 focus:text-pink-200 active:text-pink-300 hover:cursor-pointer focus:outline-none focus:ring focus:ring-teal-200"
-                    >
-                      {title}
-                    </Link>
-                  )
-                }
               </React.Fragment>
             )
           })
