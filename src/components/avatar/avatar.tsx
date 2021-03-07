12ca82fe0d2dcd82587d8e17d7d479d8771038ab
Jillian <jillianserenity@gmail.com>
2021-03-06 21:11:11 -0700

    fix: switched images to the new gatsby-plugin-image, cnverted images to StaticImage.

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -4,22 +4,20 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const Avatar: FC<ReactNode> = ({}) => {
   return (
-    <React.Fragment>
-      <div className="absolute -top-16 left-1/2">
-        <div className="relative flex flex-col items-center justify-between overflow-hidden bg-center bg-no-repeat bg-cover rounded-full shadow-xl w-28 h-28 -left-1/2">
-          <StaticImage
-            src="../../images/BIfVQUcG_400x400.jpg"
-            placeholder="blurred"
-            layout="constrained"
-            width={300}
-            formats={["auto", "webp", "avif"]}
-            className="bg-cover"
-            alt="Jillian's avatar."
-          />
-          <div className="absolute top-0 left-0 w-full h-full rounded-full ring-4 ring-purple-300 ring-opacity-30 ring-inset"></div>
-        </div>
+    <div className="absolute -top-16 left-1/2">
+      <div className="relative flex flex-col items-center justify-between overflow-hidden bg-center bg-no-repeat bg-cover rounded-full shadow-xl w-28 h-28 -left-1/2">
+        <StaticImage
+          src="../../images/BIfVQUcG_400x400.jpg"
+          placeholder="blurred"
+          layout="constrained"
+          width={300}
+          formats={["auto", "webp", "avif"]}
+          className="bg-cover"
+          alt="Jillian's avatar."
+        />
+        <div className="absolute top-0 left-0 w-full h-full rounded-full ring-4 ring-purple-300 ring-opacity-30 ring-inset"></div>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
 
