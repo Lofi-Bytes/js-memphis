1ada5a42f32b013c1066a0073f13d8b1b29d18ae
Jillian S. Estrella <jillianserenity@gmail.com>
2021-12-23 13:37:22 -0700

    fix: cleans up avatar component

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,46 +1,69 @@
 import * as React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
-// import { useStaticQuery, graphql } from 'gatsby'
-// import Img from 'gatsby-image'
 
+import {
+  formatClassList,
+  joinStrings
+} from '../../utils/utils'
+
+type AvatarProps = {
+  children: React.ReactNode,
+  className?: string
+}
+
+const AVATAR_OUTER = `
+  -top-16
+  absolute
+  left-1/2
+`
+
+const AVATAR_INNER = `
+  -left-1/2
+  h-28
+  overflow-hidden
+  relative
+  rounded-full
+  shadow-xl
+  w-28
+`
+
+const AVATAR_RING = `
+  absolute
+  h-full
+  left-0
+  ring-4
+  ring-inset
+  ring-opacity-30
+  ring-purple-300
+  rounded-full
+  top-0
+  w-full
+`
 
 const Avatar = ({
-
-}) => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     Image01: file(relativePath: { eq: "bsfSBbsadfK.jpg" }) {
-  //       childImageSharp {
-  //         fluid(maxWidth: 300, quality: 100) {
-  //           ...GatsbyImageSharpFluid_withWebp
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-
-  // const img = data.Image01.childImageSharp.fluid
+  className
+}: AvatarProps) => {
+  const formattedAvatarOuter = formatClassList(AVATAR_OUTER)
+  const formattedClassList = className
+                              ? joinStrings(' ', formattedAvatarOuter, className)
+                              : formattedAvatarOuter
+  const formattedAvatarInner = formatClassList(AVATAR_INNER)
+  const formattedAvatarRing = formatClassList(AVATAR_RING)
 
   return (
-    <React.Fragment>
-      <div className="absolute -top-16 left-1/2">
-        <div className="relative overflow-hidden rounded-full shadow-xl w-28 h-28 -left-1/2">
-          <StaticImage
-            src="../../images/bsfSBbsadfK.jpg"
-            placeholder="blurred"
-            layout="constrained"
-            width={300}
-            formats={["auto", "webp", "avif"]}
-            alt="Jillian's avatar."
-          />
-          {/* <Img
-            fluid={img}
-            alt="Jillian's avatar."
-          /> */}
-          <div className="absolute top-0 left-0 w-full h-full rounded-full ring-4 ring-purple-300 ring-opacity-30 ring-inset"></div>
-        </div>
+    <div className={formattedClassList}>
+      <div className={formattedAvatarInner}>
+        <StaticImage
+          src="../../images/bsfSBbsadfK.jpg"
+          placeholder="blurred"
+          layout="constrained"
+          width={300}
+          formats={["auto", "webp", "avif"]}
+          alt="Jillian's avatar."
+        />
+        <div className={formattedAvatarRing}></div>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
 
